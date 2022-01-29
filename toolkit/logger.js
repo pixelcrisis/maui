ad7aba5df82eb99a12becbc8913ff82faea93416
12px <d@12px.io>
2022-01-29 14:41:04 -0700

    Initial utilities, logger.

---
+++ toolkit/logger.js
@@ -0,0 +1,60 @@
+// Uses Winston to Log In Console & To File
+
+const Winston = require('winston')
+const Logbook = require('read-last-lines')
+const configs = require('../config.json').logs
+const { format, transports } = Winston
+const { combine } = format
+
+const layout = [
+	format.timestamp({ format: 'HH:mm:ss' }),
+	format.printf(log => `${log.timestamp} [${log.level}] ${log.message}`)
+]
+
+// Define Outputs
+const toFile = combine(...layout)
+const addColors = format.colorize({ all: true })
+const toConsole = combine(...layout, addColors)
+
+// Toolkits Use Global 'Self' Identifier
+module.exports = (Self, filename = 'Maui.log') => {
+	// Create The Logger
+	Winston.addColors(configs.colors)
+	const Logger = Winston.createLogger({
+		level: configs.level, 
+		levels: configs.levels, 
+		transports: [
+			new transports.File({ filename, format: toFile }),
+			new transports.Console({ format: toConsole })
+		]
+	})
+
+	// Add Our Logging Access Points
+	Self.Log = (text, type = "core") => Logger[type](text)
+	Self.Logs = async (n = 30) => await Logbook.read(filename, n)
+
+	// Send Logs To A Discord Channel
+	Self.Note = function (text, name, type = "core") {
+		// Also Actually Log Them
+		this.Log(name || text, type)
+		// Ignore If We Aren't A Bot
+		if (!this || !this.ready) return
+		// Or If We Can't Find The Channel
+		let channel = this.channels.cache.get(configs.channel)
+		if (!channel) return
+
+		let data = { channel, color: this.base.embed_color }
+		let post = { head: name || '', desc: text.stack || text }
+
+		// Add Ping/Formatting For Errors
+		if (type == "warn") {
+			Data.color = '0xFF0000'
+			post.desc = "```" + post.desc + "```"
+			post.text = `<@${this.auth.authors.join('><@')}>`
+			if (text.stack) this.Log(text.stack, type)
+		}
+
+		let Reply = this.Reply(data, post)
+		return Reply.send()
+	}
+}
---
+++ toolkit/utility.js
@@ -0,0 +1,38 @@
+// Global Helpers & Tools
+
+// Toolkits Use Global 'Self' Identifier
+module.exports = Self => {
+
+	Self._match = function (a, b) {
+		return a && b && a.toLowerCase() == b.toLowerCase()
+	}
+
+	Self._holds = function (a, b) {
+		if (!a || !b) return false
+		return a.toLowerCase().indexOf(b.toLowerCase())
+	}
+
+	Self._keyVal = function (arr) {
+		return [ arr.shift().toLowerCase(), arr.join(' ') ]
+	}
+
+	Self._arrSort = function (arr, key) {
+		const byVal = (a, b) => a > b ? 1 : -1
+		const byKey = (a, b) => a[key] > b[key] ? 1 : -1
+		return arr.sort(key ? byKey : byVal)
+	}
+
+	Self._strip = function (str) {
+		if (!str) return ''
+		let clip = str.length - 1
+		let isID = str.indexOf('<') === 0
+		let trim = isID ? 2 : 0
+
+		if (str.indexOf('@&') === 1) trim = 3
+		if (str.indexOf('@!') === 1) trim = 3
+		if (str.indexOf('<:') === 0) trim = 1
+
+		return isID ? str.substring(trim, clip) : str
+	}
+
+}
