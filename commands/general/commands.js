68458df9fc5c1ce6c40b0d0515be59ee7a506b9d
12px <d@12px.io>
2022-02-07 11:57:25 -0700

    Add commands command.

---
+++ commands/general/commands.js
@@ -0,0 +1,59 @@
+module.exports = {
+	name: 'commands',
+	also: [ 'cmd', 'cmds', 'command' ],
+	gate: 1,
+
+	help: {
+		head: "m!commands",
+		desc: "Lists the commands you have acces to."
+	},
+
+	post: {
+		head: "Maui Commands ({perm})",
+		desc: "Below is a list of all the commands you have acces to.",
+		grid: []
+	},
+
+	lang: {
+		prefix: "**{guild} Prefix:** `m!`",
+		footer: "Use `m!help command` for more info."
+	},
+
+	fire: async function (Maui, Msg) {
+		let sort = {}
+		let post = Maui.$copy(this.post)
+		let list = Maui._arrSort(Maui.allCommand(Msg))
+
+		for (let cmd of list) {
+			sort[cmd.section] = sort[cmd.section] || ''
+			sort[cmd.section] += `${ cmd.name }\n`
+		}
+		for (let name in sort) {
+			post.grid.push({ name, text: sort[name], col: 1 })
+		}
+
+		let text = `${ this.lang.prefix }\n${ this.lang.footer }`
+		post.grid.push({ text })
+		return Maui.Reply(Msg, post, { perm: Msg.access.name })
+	},
+
+	test: async function (Maui, Msg) {
+		await Maui.testCommand(Msg, this.name)
+
+		Msg.access = Maui.$badges[1]
+		await Maui.testCommand(Msg, this.name)
+
+		Msg.access = Maui.$badges[2]
+		await Maui.testCommand(Msg, this.name)
+
+		Msg.access = Maui.$badges[4]
+		await Maui.testCommand(Msg, this.name)
+
+		Msg.access = Maui.$badges[5]
+		await Maui.testCommand(Msg, this.name)
+
+		Maui.Reply(Msg, `Expected: 5 Varying Help Screens`)
+		return true
+	}
+
+}
